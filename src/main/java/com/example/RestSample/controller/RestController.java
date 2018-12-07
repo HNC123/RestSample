4d03c50ccf9e8292321b397cde24ddfd623654e4
Tarang2015 <tribhuwan.sams@gmail.com>
2018-12-07 16:34:01 +0530

    controller file

--- src/main/java/com/example/RestSample/controller/RestController.java
+++ src/main/java/com/example/RestSample/controller/RestController.java
@@ -2,9 +2,7 @@ package com.example.RestSample.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by A-8677 on 12/7/2018.
- */
+
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
