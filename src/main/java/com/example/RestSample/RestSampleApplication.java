51e8d9ba968f835d51e05c5a1e5b5e3cff7fc0a6
Tarang2015 <tribhuwan.sams@gmail.com>
2018-12-08 10:55:31 +0530

    controller file

--- src/main/java/com/example/RestSample/RestSampleApplication.java
+++ src/main/java/com/example/RestSample/RestSampleApplication.java
@@ -2,11 +2,16 @@ package com.example.RestSample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestSampleApplication {
+public class RestSampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestSampleApplication.class, args);
+	}
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestSampleApplication.class);
 	}
 }
