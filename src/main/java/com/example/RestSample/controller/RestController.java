4aaafb96b66f88f09c41adc1a3109dddb6b220c5
Tarang2015 <tribhuwan.sams@gmail.com>
2018-12-08 13:53:34 +0530

    controller file

--- src/main/java/com/example/RestSample/controller/RestController.java
+++ src/main/java/com/example/RestSample/controller/RestController.java
@@ -1,15 +1,29 @@
 package com.example.RestSample.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.RestSample.com.example.RestSample.model.Employee;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping(value = "/RestApp")
 public class RestController {
 
-    @RequestMapping(value = "/welcome")
+    @RequestMapping(value = "/welcome",method = RequestMethod.GET)
     public String getMessage(){
         return "Hello Welcome";
     }
+@RequestMapping(value="/getEmp/{sid}",method = RequestMethod.GET)
+public Employee getEmployee(@PathVariable(value = "sid") String sid){
+        System.out.print(sid);
+    Employee emp=new Employee();
 
 
+    emp.setEmail("avc@gmail.com");
+    emp.setName("and");
+    emp.setSid(100);
+
+
+    return  emp;
+}
 }
