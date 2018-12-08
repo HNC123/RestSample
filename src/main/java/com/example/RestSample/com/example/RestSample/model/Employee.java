8346dac0edf76829c15725a6cb2d31d2f942afa3
Tarang2015 <tribhuwan.sams@gmail.com>
2018-12-08 13:54:16 +0530

    controller file

---
+++ src/main/java/com/example/RestSample/com/example/RestSample/model/Employee.java
@@ -0,0 +1,35 @@
+package com.example.RestSample.com.example.RestSample.model;
+
+/**
+ * Created by A-8677 on 12/8/2018.
+ */
+public class Employee {
+    private int sid;
+    private  String name;
+    private String email;
+
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
